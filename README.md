ee614e04278b6f1f3b2f0e5942d578fb5b707873
yanbo <yanbo@yanbodeMacBook-Pro.local>
2022-01-12 08:42:47 +0800

    项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # meeting_record
-基于SpringBoot的会议记录web系统后端
+基于SpringBoot的会议记录web系统后端，对以前实验室的会议记录系统进行二次开发
+
+
---
+++ src/main/java/com/bobo/MeetingRecordMain.java
@@ -0,0 +1,11 @@
+package com.bobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRecordMain {
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingRecordMain.class);
+    }
+}
